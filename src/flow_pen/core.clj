b54386c6379fba506b689ec5d8dc8bc5a2c444a3
Jukka Villstedt <jukka.villstedt@gmail.com>
2015-06-26 23:17:20 +0300

    x

--- src/flow_pen/core.clj
+++ src/flow_pen/core.clj
@@ -1,5 +1,7 @@
 (ns flow-pen.core
-  (:require (flow-gl.gui [drawable :as drawable]
+  (:require [clojure.core.async :as async]
+            [flow-gl.csp :as csp]
+            (flow-gl.gui [drawable :as drawable]
                          [layout :as layout]
                          [layouts :as layouts]
                          [layout-dsl :as l]
@@ -27,22 +29,25 @@
            [jpen PButtonEvent PenManager PKindEvent PLevelEvent PScrollEvent PLevel PLevel$Type]))
 
 
-(def pen-event-listener
-  (proxy [PenListener] []
-    (penLevelEvent [^PLevelEvent ev]
-      (let [level-to-tuple 
-            (fn [^PLevel level]
-              (condp = (. level getType) 
-                PLevel$Type/X {:x (. level value)}
-                PLevel$Type/Y {:y (. level value)}
-                PLevel$Type/PRESSURE {:p (. level value)}
-                {}))
-            values (reduce merge {} (map level-to-tuple (. ev levels)))]
-        (println (:x values) (:y values) (:p values))))
-    (penButtonEvent [ev])
-    (penKindEvent [ev])
-    (penScrollEvent [ev])
-    (penTock [millis])))
+(defn pen-event-listener [pen-event-channel]
+  (let [values (atom {})]
+    (proxy [PenListener] []
+      (penLevelEvent [^PLevelEvent p-level-event]
+        (swap! values (fn [values]
+                        (reduce (fn [values level]
+                                  (let [key (condp = (.getType level) 
+                                              PLevel$Type/X :x
+                                              PLevel$Type/Y :y
+                                              PLevel$Type/PRESSURE :p
+                                              (.getType level))]
+                                    (assoc values key (.value level))))
+                                values
+                                (.levels p-level-event))))
+        (async/put! pen-event-channel @values))
+      (penButtonEvent [p-level-event])
+      (penKindEvent [p-level-event])
+      (penScrollEvent [p-level-event])
+      (penTock [millis]))))
 
 
 
@@ -56,45 +61,29 @@
                     color)))
 
 (defn paint-view [view-context state]
-  (text "as fasf asdf asf asdf asdf ads faas fas fasdf"))
+  (text (str "pen:" (:pen-state state))))
 
-(defn paint [view-context]
-  {:local-state {}
-   :view #'paint-view})
+(defn create-paint [pen-event-channel]
+  (fn [view-context]
+    (async/go-loop []
+      (let [pen-states (csp/drain pen-event-channel nil)]
+        (if pen-states
+          (do (gui/apply-to-state view-context assoc :pen-state (last pen-states))
+              (recur)))))
+
+    {:local-state {:pen-state {}}
+     :view #'paint-view}))
 
 
 (defn start []
   (.start (Thread. (fn []
-                     (gui/start-control paint (fn [canvas]
-                                                (let [pen-manager (new PenManager canvas)]
-                                                  (.. pen-manager pen (addListener pen-event-listener)))))))))
+                     (let [pen-event-channel (async/chan 100)]
+                       (gui/start-control (create-paint pen-event-channel)
+                                          (fn [canvas]
+                                            (let [pen-manager (new PenManager canvas)]
+                                              (.. pen-manager pen (addListener (pen-event-listener pen-event-channel)))))))))))
 
 
-#_(defn start2 []
-  (let [frame (Frame. "Juu")
-        caps (new GLCapabilities (GLProfile/get GLProfile/GL2))
-        chooser (new DefaultGLCapabilitiesChooser)]
-    (. caps setDoubleBuffered true)
-    (. caps setHardwareAccelerated true)
-    (. caps setSampleBuffers true)
-    (. caps setNumSamples 4)
-    (let [gl-canvas (new GLCanvas caps chooser nil nil)
-          pen-manager (new PenManager gl-canvas)]
-      (. gl-canvas addGLEventListener gl-event-listener)
-      (.. pen-manager pen (addListener pen-event-listener))
-      (. frame add gl-canvas)
-      (let [pixels (int-array 256)
-            image (. (Toolkit/getDefaultToolkit) createImage (new MemoryImageSource 16 16 pixels 0 16))
-            cursor (. (Toolkit/getDefaultToolkit) createCustomCursor image (new Point 0 0) "invisibleCursor")]
-        (. frame setCursor cursor))
-      (. frame setSize 300 300)
-      (. frame
-         (addWindowListener
-          (proxy [WindowAdapter] []
-            (windowClosing [event]
-              (println "closing")
-              (. System exit 0)))))
-      (println "showing")
-      (.setVisible frame  true))))
+
 
 
