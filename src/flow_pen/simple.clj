95592f98dd8a7e72d6f8e5fa172a79c65a7298de
Jukka Villstedt <jukka.villstedt@gmail.com>
2015-08-03 23:49:24 +0300

    x

--- src/flow_pen/simple.clj
+++ src/flow_pen/simple.clj
@@ -42,18 +42,29 @@
 (defn pen-event-listener [pen-event-channel]
   (let [values (atom {})]
     (proxy [PenListener] []
+      #_(penLevelEvent [^PLevelEvent p-level-event]
+          (swap! values (fn [values]
+                          (reduce (fn [values level]
+                                    (let [key (condp = (.getType level) 
+                                                PLevel$Type/X :x
+                                                PLevel$Type/Y :y
+                                                PLevel$Type/PRESSURE :p
+                                                (.getType level))]
+                                      (assoc values key (.value level))))
+                                  values
+                                  (.levels p-level-event))))
+          (async/put! pen-event-channel @values))
       (penLevelEvent [^PLevelEvent p-level-event]
-        (swap! values (fn [values]
-                        (reduce (fn [values level]
-                                  (let [key (condp = (.getType level) 
-                                              PLevel$Type/X :x
-                                              PLevel$Type/Y :y
-                                              PLevel$Type/PRESSURE :p
-                                              (.getType level))]
-                                    (assoc values key (.value level))))
-                                values
-                                (.levels p-level-event))))
-        (async/put! pen-event-channel @values))
+        (async/put! pen-event-channel
+                    (reduce (fn [values level]
+                              (let [key (condp = (.getType level) 
+                                          PLevel$Type/X :x
+                                          PLevel$Type/Y :y
+                                          PLevel$Type/PRESSURE :p
+                                          (.getType level))]
+                                (assoc values key (.value level))))
+                            {}
+                            (.levels p-level-event))))
       (penButtonEvent [p-level-event])
       (penKindEvent [p-level-event])
       (penScrollEvent [p-level-event])
@@ -126,11 +137,14 @@
   float texture_x = int(texture_coordinate.x * 400.0);
   float texture_y = int((1 - texture_coordinate.y) * 400.0);
   
-  if(texture_x == x && texture_y == y)
+  if(texture_x == int(x) && texture_y == int(y))
   {
-  outColor = vec4(color.r + 0.7 , color.g, color.b, color.a);
+  outColor = vec4(1.0 , color.g, color.b, 1.0);
+  //outColor = vec4(1.0, 1.0, 1.0, 1.0);
   } else {
-  outColor = color;
+  outColor = vec4(color.r , color.g, color.b, 1.0);;
+
+
   }
 
 
@@ -188,81 +202,93 @@
 
 
 
+(defn render-pen-state [gpu-state window pen-state]
+  (window/with-gl window gl
+    (let [width 400
+          height 400
+          render-target-1 (or (:paint-render-target-1 gpu-state)
+                              (render-target/create width height gl))
+          
+          render-target-2 (or (:paint-render-target-2 gpu-state)
+                              (render-target/create width height gl))
+
+          ;;_ (println (:x pen-state) (:y pen-state))
+
+          renderers (renderer/map-for-renderers renderer/start-frame gl (:renderers gpu-state))
+          renderers (render-target/render-to render-target-2 gl
+                                             #_(opengl/clear gl 0 255 0 255)
+                                             (renderer/render-frame-drawables
+                                              [(drawable/->Quad ["texture" (:texture render-target-1)]
+                                                                [:1f "resolution" width
+                                                                 :1f "x" (:x pen-state)
+                                                                 :1f "y" (:y pen-state)]
+                                                                paint-fragment-shader-source
+                                                                0 0 width height)]
+                                              gl
+                                              renderers))
+          _ (opengl/clear gl 0 0 0 1)
+          renderers (renderer/render-frame-drawables
+                     [(drawable/->Quad ["texture" #_(:texture gpu-state) (:texture render-target-2)]
+                                       []
+                                       quad/fragment-shader-source
+                                       0 0 400 400)
+                      (assoc (text (str "x: " (:x pen-state)))
+                             :x 0 :y 0 :width 100 :height 100)]
+                     gl renderers)
+          
+          renderers (renderer/map-for-renderers renderer/end-frame gl renderers)]
+
+      (assoc gpu-state
+             :renderers renderers
+             :paint-render-target-1 render-target-2
+             :paint-render-target-2 render-target-1))))
+
+(defn event-loop []
+  (let [pen-event-channel (async/chan)
+        pen-state-channel (async/chan 100)
+        window (jogl-window/create 400
+                                   400
+                                   :profile :gl3
+                                   :init opengl/initialize
+                                   :close-automatically true
+                                   :use-awt true
+                                   :awt-init (fn [canvas]
+                                               (let [pen-manager (new PenManager canvas)]
+                                                 (.. pen-manager pen (addListener (pen-event-listener pen-event-channel))))))]
+    (println "window created")
+    (async/go-loop [pen-state {}]
+      (let [levels (async/<! pen-event-channel)
+            pen-state (conj pen-state levels)]
+        (async/>! pen-state-channel pen-state)
+        (recur pen-state)))
+
+    (try
+      (loop [gpu-state {:renderers (window/with-gl window gl
+                                     [(renderer/create-quad-renderer gl)
+                                      (renderer/create-quad-view-renderer gl)])
+                        :texture (window/with-gl window gl
+                                   (texture/create-for-file "pumpkin.png" gl))}]
+        (println "loop")
+        (let [pen-states #_(async/<!! pen-state-channel) (csp/drain pen-state-channel nil)]
+          (if pen-states
+            (let [gpu-state (render-pen-state gpu-state window (last pen-states))] 
+              (window/swap-buffers window)
+              (recur gpu-state)))))
+
+      (println "exiting")
+      (catch Exception e
+        (println "exception")
+        (window/close window)
+        (throw e)))))
+
 (defn start []
-  (.start (Thread. (fn []
-                     (let [pen-event-channel (async/chan 100)
-                           window (jogl-window/create 400
-                                                      400
-                                                      :profile :gl3
-                                                      :init opengl/initialize
-                                                      :close-automatically true
-                                                      :use-awt true
-                                                      :awt-init (fn [canvas]
-                                                                  (let [pen-manager (new PenManager canvas)]
-                                                                    (.. pen-manager pen (addListener (pen-event-listener pen-event-channel))))))
-                           
-                           texture (window/with-gl window gl
-                                     (texture/create-for-file "pumpkin.png" gl))]
+  
+  #_(.start (Thread. (fn []
+                       (trace/untrace-ns 'flow-pen.simple)
+                       (trace/trace-var* 'flow-pen.simple/render-pen-state)
+                       (trace/with-trace
+                         (event-loop)))))
 
-                       (try
-                         (loop [gpu-state {:renderers [(window/with-gl window gl
-                                                         (renderer/create-quad-renderer gl))]}]
-                           (let [pen-states (csp/drain pen-event-channel nil)]
-                             (if pen-states
-                               (do (let [pen-state (last pen-states)
-                                         gpu-state (window/with-gl window gl
-                                                     (let [width 400
-                                                           height 400
-                                                           render-target-1 (or (:paint-render-target-1 gpu-state)
-                                                                               (render-target/create width height gl))
-                                                           
-                                                           render-target-2 (or (:paint-render-target-2 gpu-state)
-                                                                               (render-target/create width height gl))
-
-                                                           _ (println (:x pen-state) (:y pen-state))
-                                                           
-                                                           renderers (render-target/render-to render-target-2 gl
-                                                                                              #_(opengl/clear gl 0 255 0 255)
-                                                                                              (renderer/render-frame [(drawable/->Quad ["texture" (:texture render-target-1)]
-                                                                                                                                       [:1f "resolution" width
-                                                                                                                                        :1f "x" (:x pen-state)
-                                                                                                                                        :1f "y" (:y pen-state)]
-                                                                                                                                       paint-fragment-shader-source
-                                                                                                                                       0 0 width height)]
-                                                                                                                     gl (:renderers gpu-state)))
-                                                           _ (opengl/clear gl 0 0 0 1)
-                                                           renderers (renderer/render-frame [(drawable/->Quad ["texture" #_texture (:texture render-target-2)]
-                                                                                                              []
-                                                                                                              quad/fragment-shader-source
-                                                                                                              0 0 400 400)]
-                                                                                            gl renderers)]
-
-                                                       
-                                                       (assoc gpu-state
-                                                              :renderers renderers
-                                                              :paint-render-target-1 render-target-2
-                                                              :paint-render-target-2 render-target-1)))]
-
-                                     (window/swap-buffers window)
-                                     (recur gpu-state))))))
-                         #_(loop []
-                             (let [frame-started (System/currentTimeMillis)]
-                               (let [drawable (drawable-for-time texture frame-started)]
-                                 (window/with-gl window gl
-                                   (opengl/clear gl 0 0 0 1)
-                                   (swap! renderers
-                                          (fn [renderers]
-                                            (renderer/render-frame [drawable] gl renderers))))
-                                 (window/swap-buffers window))
-
-                               (when (window/visible? window)
-                                 (do (wait-for-next-frame frame-started)
-                                     (recur)))))
-
-                         (println "exiting")
-                         (catch Exception e
-                           (println "exception")
-                           (window/close window)
-                           (throw e))))))))
+  (.start (Thread. event-loop)))
 
 
